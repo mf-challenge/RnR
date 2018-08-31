a4d98c7be5d52f2fc7a4d114c5e02e308472e8c0
Praveen Kumar <praveen.kumar4@hpe.com>
2018-08-31 10:20:53 +0530

    Added redirection to home after login

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { User } from '../entity/user';
 import { Web3EmbeddedService } from '../web3-embedded.service';
 import { UserManagementService } from '../user-management.service'
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private web3: Web3EmbeddedService, private userManagementService: UserManagementService) { }
+  constructor(private web3: Web3EmbeddedService, private userManagementService: UserManagementService, private router: Router) { }
 
   registerUser(user: User) {
     // Register a wallet
@@ -27,6 +28,7 @@ export class UserService {
     // Get user meta data
     this.userManagementService.authenticateUser(user).subscribe(resp  => {
        console.log(resp);
+       this.router.navigate(['/home']);
      });
   }
 }
